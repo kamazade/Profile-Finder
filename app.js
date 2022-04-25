592c162b7b4e155896ccbdf1f120091765acc7e5
Alper KAMA <92380837+kamazade@users.noreply.github.com>
2022-04-26 00:59:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const profile = new Profile();
+const ui = new UI();
+
+
+const searchProfile  = document.querySelector("#searchProfile");
+
+searchProfile.addEventListener("keyup",(event)=>{
+    ui.clear();
+    let text = event.target.value;
+    
+    if(text!== ""){
+        profile.getProfile(text)
+          .then(res=>{
+            if(res.profile.length !== 0 ){
+                ui.showProfile(res.profile[0]);
+
+            }else{
+                ui.showAlert(text);
+            }  
+            
+            
+          })
+
+    }
+
+
+})
